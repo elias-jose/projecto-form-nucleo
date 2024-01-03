373929405fc62a2e373fe4554b660856f0489246
elias-jose <108912809+elias-jose@users.noreply.github.com>
2024-01-03 16:30:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ Usando esta funcao é possivel capturar os elementos pela sua classe
 const by = (selector) => document.querySelector(selector)
 //Capturando os elementos necessarios.
 const form = by('.form')
-const btnShow = by('.show')
+const btnShow = by('.open')
 const reset = by('.reset')
+const data = by('#data')
+const close = by('.close')
 
 //Adicionar eventos quando os botões forem clicados para alternar entre as telas.
 btnShow.addEventListener('click', function () {
@@ -22,5 +24,33 @@ reset.addEventListener('click', function () {
    form.classList.remove('show')
    form.classList.add('hide')
 })
+
+close.addEventListener('click', function () {
+   data.reset()
+   
+   btnShow.parentElement.classList.remove('hide')
+   btnShow.parentElement.classList.add('show')
+   form.classList.remove('show')
+   form.classList.add('hide')
+   
+   by('.modal').classList.remove('show')
+})
+
+data.addEventListener('submit', abrirmodal, false)
+
+function abrirmodal (evt) {
+   evt.preventDefault()
+   
+   by('.n').textContent = by('#nome').value
+   by('.m').textContent = by('#numero').value
+   by('.p').textContent = by('#prov').value
+   
+   by('.modal').classList.add('show')
+   form.classList.remove('show')
+   form.classList.add('hide')
+   
+   
+   
+}
 
 //console.log(btnShow.parentElement.classList)
